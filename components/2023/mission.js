56993dbe78e563dbbb5c84357c1d4072a0a89d1b
Josh Dzielak <jdzielak@gmail.com>
2023-04-14 18:00:10 +0200

    Add cards

--- components/2023/mission.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import c from "components/2023/common";
-
-export default function Mission() {
-  return <div className="text-lg">The Mission</div>;
-}
--- components/2023/selection.js
+++ components/2023/selection.js
@@ -1,29 +1,33 @@
 import React from "react";
 import c from "components/2023/common";
 import Member from "components/2023/member_new";
-import Mission from "components/2023/mission";
 import OrbitLevel from "components/2023/orbit_level";
-import Welcome from "components/2023/welcome";
+import WelcomeCard from "content/cards/welcome.mdx";
+import MissionCard from "content/cards/mission.mdx";
 
 export default function Selection({ selection }) {
   var markup = null;
   if (!selection) {
-    markup = <Welcome />;
+    markup = <WelcomeCard />;
   } else if (selection.name === "Mission") {
-    markup = <Mission />;
+    markup = <MissionCard />;
   } else if (selection.distance) {
     markup = <OrbitLevel selection={selection} />;
   } else if (selection.orbit) {
     markup = <Member selection={selection} />;
+  } else if (selection === {}) {
+    markup = null;
   }
 
   return (
     <div className="flex absolute right-0 bottom-0 z-10 flex-col justify-start px-3 py-4 space-y-6 pointer-events-none">
-      <div className={`${c.panelBackgroundClasses} bg-opacity-80 rounded`}>
-        <div className="flex relative flex-col py-3 px-4 pointer-events-auto">
-          {markup}
+      {markup && (
+        <div className={`${c.panelBackgroundClasses} bg-opacity-80 rounded`}>
+          <div className="flex relative flex-col py-3 px-4 pointer-events-auto">
+            {markup}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
