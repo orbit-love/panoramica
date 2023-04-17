6b962d2e514ca8ed999590246f29a98e76b76a76
Josh Dzielak <jdzielak@gmail.com>
2023-04-17 17:19:15 +0200

    Tweaks

--- components/2023/member_new.js
+++ components/2023/member_new.js
@@ -4,10 +4,14 @@ import c from "components/2023/common";
 export default function Member({ selection }) {
   return (
     <>
-      <div className="text-xl font-bold">{selection.name}</div>
-      <div className="text-sm">Orbit Level: {selection.level}</div>
-      <div className="text-sm">Love: {selection.love}</div>
-      <div className="text-sm">Reach: {selection.reach}</div>
+      <div className="mb-2 text-xl font-bold">{selection.name}</div>
+      <div className="flex flex-wrap space-x-4">
+        <div className="text-sm font-semibold">
+          Orbit Level: {selection.level}
+        </div>
+        <div className="text-sm font-semibold">Love: {selection.love}</div>
+        <div className="text-sm font-semibold">Reach: {selection.reach}</div>
+      </div>
       {selection.description && (
         <div className="my-3 text-sm leading-tight">
           {selection.description}
