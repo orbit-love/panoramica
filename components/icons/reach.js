1bbe3b298f92e2a7be47d7c87aa1a23dbac98bb6
Josh Dzielak <jdzielak@gmail.com>
2023-05-04 19:00:16 +0200

    Remove reach title

--- components/icons/reach.js
+++ components/icons/reach.js
@@ -10,7 +10,6 @@ export default function ReachIcon({ value, classes }) {
       key={1}
       icon="circle"
       style={{ opacity: 1, color: selectedColor, fontSize: "8px" }}
-      title={value.toString()}
     />
   );
   markup.push(
@@ -22,7 +21,6 @@ export default function ReachIcon({ value, classes }) {
         fontSize: "15px",
         color: value > 1 ? selectedColor : c.indigo900,
       }}
-      title={value.toString()}
     />
   );
   markup.push(
@@ -34,7 +32,6 @@ export default function ReachIcon({ value, classes }) {
         fontSize: "23px",
         color: value == 3 ? selectedColor : c.indigo900,
       }}
-      title={value.toString()}
     />
   );
   return <div className="flex items-center space-x-[2px]">{markup}</div>;
