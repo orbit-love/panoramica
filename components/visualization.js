832183234ade2fea9701f93b68dbd4f58519186b
Josh Dzielak <jdzielak@gmail.com>
2023-05-13 08:46:48 +0200

    Bring widgets into its own component

--- components/visualization.js
+++ components/visualization.js
@@ -3,9 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import c from "lib/common";
 import helper from "lib/orbitHelper";
-import Controls from "components/controls";
-import Steps from "components/steps";
-import Member from "content/cards/member";
+import Widgets from "components/widgets";
 
 export default function Orbits({
   width,
@@ -28,7 +26,6 @@ export default function Orbits({
   const prevHeight = c.usePrevious(height);
 
   const svgRef = useRef();
-  const [expanded, setExpanded] = useState(true);
   const [animate, setAnimate] = useState(!isReduced);
   const [cycle, setCycle] = useState(true);
   const [selection, setSelection] = useState(null);
@@ -75,6 +72,7 @@ export default function Orbits({
     }
 
     console.log(`Drawing ${members.length()} members...`);
+
     const props = {
       svgRef,
       width,
@@ -115,7 +113,6 @@ export default function Orbits({
     levels,
   ]);
 
-  const classes = `flex space-x-3 rounded-lg text-[${c.whiteColor}] bg-[${c.backgroundColor}] border border-indigo-800 bg-opacity-70`;
   return (
     <>
       <div>
@@ -126,65 +123,21 @@ export default function Orbits({
         ></svg>
       </div>
       <div className="hidden bg-[#0F0A25] text-[#eef2ff]" />
-      <div
-        className={`flex absolute bottom-0 left-0 z-10 flex-col px-4 py-5 space-y-4`}
-      >
-        {expanded && members && (
-          <div className={`${classes} w-96`}>
-            <Steps
-              svgRef={svgRef}
-              selection={selection}
-              setSelection={setSelection}
-              members={members}
-              setMembers={setMembers}
-              step={step}
-              setStep={setStep}
-              setCycle={setCycle}
-            />
-          </div>
-        )}
-        <div className="flex">
-          <div className={`${classes} py-4 px-5 pointer-events-auto`}>
-            {expanded && (
-              <button onClick={() => setExpanded(false)} className="btn">
-                <FontAwesomeIcon
-                  icon="chevron-down"
-                  className="text-lg"
-                ></FontAwesomeIcon>
-              </button>
-            )}
-            {!expanded && (
-              <button onClick={() => setExpanded(true)} className="btn">
-                <FontAwesomeIcon
-                  icon="solar-system"
-                  className="text-lg"
-                ></FontAwesomeIcon>
-              </button>
-            )}
-            <div className="border border-indigo-900" />
-            <Controls
-              animate={animate}
-              setAnimate={setAnimate}
-              cycle={cycle}
-              setCycle={setCycle}
-              fullscreen={fullscreen}
-              setFullscreen={setFullscreen}
-            />
-          </div>
-          <div className="mx-auto" />
-        </div>
-      </div>
-      {selection && selection.level && (
-        <div
-          className={`absolute right-0 bottom-0 z-10 px-4 py-5 w-96 pointer-events-none`}
-        >
-          <div
-            className={`${classes} flex relative flex-col px-7 py-8 pointer-events-auto`}
-          >
-            <Member selection={selection} />
-          </div>
-        </div>
-      )}
+      <Widgets
+        svgRef={svgRef}
+        members={members}
+        setMembers={setMembers}
+        step={step}
+        setStep={setStep}
+        selection={selection}
+        setSelection={setSelection}
+        fullscreen={fullscreen}
+        setFullscreen={setFullscreen}
+        animate={animate}
+        setAnimate={setAnimate}
+        cycle={cycle}
+        setCycle={setCycle}
+      />
     </>
   );
 }
---
+++ components/widgets.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import c from "lib/common";
+import Controls from "components/controls";
+import Member from "content/cards/member";
+import Steps from "components/steps";
+
+export default function Widgets({
+  svgRef,
+  members,
+  setMembers,
+  selection,
+  setSelection,
+  step,
+  setStep,
+  fullscreen,
+  setFullscreen,
+  animate,
+  setAnimate,
+  cycle,
+  setCycle,
+}) {
+  const [expanded, setExpanded] = useState(true);
+
+  const classes = `flex space-x-3 rounded-lg text-[${c.whiteColor}] bg-[${c.backgroundColor}] border border-indigo-800 bg-opacity-80`;
+  return (
+    <>
+      <div
+        className={`flex absolute bottom-0 left-0 z-10 flex-col px-4 py-5 space-y-4`}
+      >
+        {expanded && members && (
+          <div className={`${classes} w-96`}>
+            <Steps
+              svgRef={svgRef}
+              selection={selection}
+              setSelection={setSelection}
+              members={members}
+              setMembers={setMembers}
+              step={step}
+              setStep={setStep}
+              setCycle={setCycle}
+            />
+          </div>
+        )}
+        <div className="flex">
+          <div className={`${classes} py-4 px-5 pointer-events-auto`}>
+            {expanded && (
+              <button onClick={() => setExpanded(false)} className="btn">
+                <FontAwesomeIcon
+                  icon="chevron-down"
+                  className="text-lg"
+                ></FontAwesomeIcon>
+              </button>
+            )}
+            {!expanded && (
+              <button onClick={() => setExpanded(true)} className="btn">
+                <FontAwesomeIcon
+                  icon="solar-system"
+                  className="text-lg"
+                ></FontAwesomeIcon>
+              </button>
+            )}
+            <div className="border border-indigo-900" />
+            <Controls
+              animate={animate}
+              setAnimate={setAnimate}
+              cycle={cycle}
+              setCycle={setCycle}
+              fullscreen={fullscreen}
+              setFullscreen={setFullscreen}
+            />
+          </div>
+          <div className="mx-auto" />
+        </div>
+      </div>
+      {selection && selection.level && (
+        <div
+          className={`absolute right-0 bottom-0 z-10 px-4 py-5 w-96 pointer-events-none`}
+        >
+          <div
+            className={`${classes} flex relative flex-col px-7 py-8 pointer-events-auto`}
+          >
+            <Member selection={selection} />
+          </div>
+        </div>
+      )}
+      <div className="mx-auto" />
+    </>
+  );
+}
