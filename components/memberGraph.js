402688b7c56c3a8aad97014272d707f1fb862d30
Josh Dzielak <jdzielak@gmail.com>
2023-05-27 16:10:18 +0200

    Increase network view size

--- components/memberGraph.js
+++ components/memberGraph.js
@@ -95,7 +95,7 @@ export default function MemberGraph({
   }, [showNetwork, members]);
 
   const graphWidth = width * 0.95;
-  const graphHeight = height * 0.9;
+  const graphHeight = height * 0.95;
 
   // the graphWidth + 5 prevents the canvas from overflowing the modal
   return (
