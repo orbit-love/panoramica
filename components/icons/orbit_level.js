606ceef9cf3f01dec76cef45d060849ef1efea9a
Josh Dzielak <jdzielak@gmail.com>
2023-05-28 15:27:18 +0200

    Improve OL icon situation

--- components/icons/orbit_level.js
+++ components/icons/orbit_level.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import c from "lib/common";
 
-export default function OrbitLevelIcon({ level, classes }) {
+export default function OrbitLevelIcon({ number, classes }) {
+  const color = c.orbitLevelColorScale(number);
   return (
-    <>
-      <FontAwesomeIcon icon={`circle-${level}`} className={classes} />
-    </>
+    <FontAwesomeIcon
+      icon={`circle-${number}`}
+      className={classes}
+      style={{
+        color,
+      }}
+    />
   );
 }
