196c1a221b70bb938f60b5ffc07b0b7fe3d24c20
Josh Dzielak <jdzielak@gmail.com>
2023-06-21 23:22:33 +0200

    Improve connection sorting

--- components/compact/connections.js
+++ components/compact/connections.js
@@ -9,45 +9,67 @@ export default function CompactConnections({
   connection,
   setConnection,
 }) {
-  const sortByMentions = (a, b) => {
-    var [outgoingA, incomingA] = member.connections[a];
-    var [outgoingB, incomingB] = member.connections[b];
-    return outgoingB + incomingB - (outgoingA + incomingA);
+  // if the connection doesn't exist, right now it could
+  // be because the time slider reducer and some members aren't there
+  // that's bad though, so this is a protection for now
+  const mapActor = (actor) => community.findMemberByActor(actor);
+
+  const sortByFields = (a, b) => {
+    var [outgoingA, incomingA] = member.connections[a.globalActor];
+    var [outgoingB, incomingB] = member.connections[b.globalActor];
+    var connectionStrengthSort =
+      outgoingB + incomingB - (outgoingA + incomingA);
+    var orbitLevelSort = a.level - b.level;
+
+    // last resort, sort by name
+    var nameSort = 0,
+      nameA = a.globalActorName?.toLowerCase(),
+      nameB = b.globalActorName?.toLowerCase();
+    if (nameA < nameB) {
+      nameSort = -1;
+    }
+    if (nameA > nameB) {
+      nameSort = 1;
+    }
+    return connectionStrengthSort || orbitLevelSort || nameSort;
   };
+
   const mutuals = Object.keys(member.connections)
     .filter((actor) => {
       var [outgoing, incoming] = member.connections[actor];
       return outgoing > 0 && incoming > 0;
     })
-    .sort(sortByMentions);
+    .map(mapActor)
+    .filter((a) => a)
+    .sort(sortByFields);
+
   const outgoing = Object.keys(member.connections)
     .filter((actor) => {
       var [outgoing, incoming] = member.connections[actor];
       return outgoing > 0 && incoming === 0;
     })
-    .sort(sortByMentions);
+    .map(mapActor)
+    .filter((a) => a)
+    .sort(sortByFields);
+
   const incoming = Object.keys(member.connections)
     .filter((actor) => {
       var [outgoing, incoming] = member.connections[actor];
       return outgoing === 0 && incoming > 0;
     })
-    .sort(sortByMentions);
+    .map(mapActor)
+    .filter((a) => a)
+    .sort(sortByFields);
 
   const Connections = ({ name, connections }) => {
-    // if the connection doesn't exist, right now it could
-    // be because the time slider reducer and some members aren't there
-    // that's bad though, so this is a protection for now
-    var connectedMembers = connections
-      .map((actor) => community.findMemberByActor(actor))
-      .filter((m) => m);
     return (
       <>
-        {connectedMembers.length > 0 && (
+        {connections.length > 0 && (
           <div className="flex flex-col space-y-3">
             <div className="flex flex-col pb-2">
               <div className="pb-1 font-semibold text-indigo-400">{name}</div>
               <div className="flex flex-col">
-                {connectedMembers.map((connectedMember) => (
+                {connections.map((connectedMember) => (
                   <CompactConnection
                     key={connectedMember.id}
                     member={member}
