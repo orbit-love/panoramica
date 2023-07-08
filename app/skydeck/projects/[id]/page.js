2c9b44f8fc3946c62ec24af309e2b521a84008c0
Josh Dzielak <jdzielak@gmail.com>
2023-07-08 18:18:34 +0200

    Fix import

--- app/skydeck/projects/[id]/page.js
+++ app/skydeck/projects/[id]/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "app/api/auth/[...nextauth]/route";
+import { prisma } from "lib/db";
 
 import Wrapper from "components/wrapper";
 import GraphConnection from "lib/graphConnection";
--- components/skydeck/fetches.js
+++ components/skydeck/fetches.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const headers = {
   Accept: "application/json",
   "Content-Type": "application/json",
