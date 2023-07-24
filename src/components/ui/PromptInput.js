7fd2b40ebaf3bfd3b183942b9a0a27f61c2dec39
Josh Dzielak <jdzielak@gmail.com>
2023-07-24 17:50:47 +0200

    Remove rows hardcoding on PromptInput textarea
    Add right padding on desktop to avoid text flowing into the button

--- src/components/ui/PromptInput.js
+++ src/components/ui/PromptInput.js
@@ -21,15 +21,14 @@ export default function PromptInput(props) {
       className="flex flex-col space-y-3 w-full"
     >
       <div
-        className="relative flex h-full flex-1 items-stretch md:flex-col"
+        className="flex relative flex-1 items-stretch h-full md:flex-col"
         role="presentation"
       >
-        <div className="flex flex-col w-full py-[10px] flex-grow md:py-4 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:bg-gray-700 rounded shadow-xs dark:shadow-xs">
+        <div className="flex flex-col w-full py-[10px] flex-grow md:py-4 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:bg-gray-700 rounded-md shadow-xs dark:shadow-xs">
           <textarea
             ref={textareaRef}
             value={prompt}
-            rows={3}
-            className="m-0 w-full resize-none border-0 bg-transparent p-0 pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pr-20 pl-3 md:pl-0 dark:text-white"
+            className="p-0 pl-3 pr-10 m-0 w-full bg-transparent border-0 resize-none focus:ring-0 focus-visible:ring-0 md:pl-0 md:pr-24 dark:text-white dark:bg-transparent"
             placeholder={placeholder}
             disabled={disabled ? "disabled" : ""}
             onChange={(e) => setPrompt(e.target.value)}
