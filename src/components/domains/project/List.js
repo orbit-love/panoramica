553e1cf0277c320465adae8d8dfd434307e3f589
Josh Dzielak <jdzielak@gmail.com>
2023-08-14 15:01:17 +0200

    Add option to repair (re-import) project on dashboard
    For admins

--- src/components/domains/project/List.js
+++ src/components/domains/project/List.js
@@ -2,17 +2,29 @@ import React from "react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 
+import { putProjectImport } from "src/data/client/fetches";
+
 export default function List({ projects, projectUrl }) {
   const { data: session } = useSession();
   const user = session?.user;
+
+  const importProject = async (project) => {
+    putProjectImport({
+      project,
+      setLoading: () => {},
+      onSuccess: () => {
+        console.log("Success: data has been re-imported.");
+      },
+    });
+  };
 
   return (
     <div className="flex flex-col items-baseline space-y-2">
       <div className="flex items-baseline space-x-2">
         <div className="text-lg font-bold">Choose a Project</div>
       </div>
-      {projects?.length === 0 && <div>None</div>}
-      {projects?.map((project) => (
+      {projects.length === 0 && <div>None</div>}
+      {projects.map((project) => (
         <div className="flex justify-between w-full" key={project.id}>
           <div className="flex space-x-2">
             <Link
@@ -23,7 +35,17 @@ export default function List({ projects, projectUrl }) {
               <span>{project.name}</span>
             </Link>
           </div>
-          {user?.admin && <span>{`${project.prismaUser.email}`}</span>}
+          {user.admin && (
+            <div className="flex space-x-4">
+              <span>{`${project.prismaUser.email}`}</span>
+              <button
+                className="underline"
+                onClick={() => importProject(project)}
+              >
+                Repair
+              </button>
+            </div>
+          )}
         </div>
       ))}
     </div>
