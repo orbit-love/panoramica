a3b24e3b418f0ef94b60c9017b12e12dbc920471
Josh Dzielak <jdzielak@gmail.com>
2023-09-02 18:42:51 +0200

    Add property filters to public schema

--- src/graphql/welcome/resolvers.js
+++ src/graphql/welcome/resolvers.js
@@ -1,8 +1,18 @@
 import getSimilarConversations from "src/graphql/resolvers/getSimilarConversations";
 import searchConversations from "src/graphql/resolvers/searchConversations";
+import resolvePropertyFilters from "src/graphql/resolvers/project/propertyFilters";
 
 const resolvers = {
   Project: {
+    async propertyFilters(parent, args, { resolveTree }) {
+      const projectId = resolveTree.args.where.id;
+      const { propertyNames, where } = args;
+      return resolvePropertyFilters({
+        projectId,
+        propertyNames,
+        where,
+      });
+    },
     async searchConversations(parent, args) {
       const { id: projectId } = parent;
       const { query } = args;
