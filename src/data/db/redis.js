2f465067abc3773f04e6d821c39b6221c533fa15
Josh Dzielak <jdzielak@gmail.com>
2023-09-13 13:43:31 +0200

    Test to see if option is not necessary

--- src/data/db/redis.js
+++ src/data/db/redis.js
@@ -6,7 +6,5 @@ var finalRedisUrl = REDIS_URL;
 if (NODE_ENV === "production") {
   // replace the redis:// with rediss://
   finalRedisUrl = REDIS_URL.replace("redis://", "rediss://");
-  console.log("Redis URL:", finalRedisUrl);
 }
-const options = NODE_ENV === "production" ? { tls: {} } : {};
 export const connection = new Redis(finalRedisUrl, options);
