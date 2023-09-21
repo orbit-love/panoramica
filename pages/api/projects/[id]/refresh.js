7254ed0f5a94b87bcdb497101e112a61e64dc179
Josh Dzielak <jdzielak@gmail.com>
2023-09-21 16:14:56 +0200

    Remove indexing from refresh for now

--- pages/api/projects/[id]/refresh.js
+++ pages/api/projects/[id]/refresh.js
@@ -1,12 +1,8 @@
 import { graph } from "src/data/db";
-import { check, redirect, authorizeProject, createJWT } from "src/auth";
-import { aiReady } from "src/integrations/ready";
+import { check, redirect, authorizeProject } from "src/auth";
 import { syncActivities } from "src/data/graph/mutations";
 import { getAPIUrl, fetchActivitiesPage } from "src/integrations/orbit/api";
 import { orbitImportReady } from "src/integrations/ready";
-import { indexConversations } from "src/integrations/typesense";
-
-import { ogm, Project } from "src/models";
 
 export default async function handler(req, res) {
   const user = await check(req, res);
@@ -25,17 +21,11 @@ export default async function handler(req, res) {
       return res.status(400).json({ message: "Could not refresh project" });
     }
 
-    let { id: projectId, url, apiKey, workspace } = project;
+    let { url, apiKey, workspace } = project;
     // if a url is not provided, use the default
     if (!url) {
       url = getAPIUrl({ workspace });
     }
-
-    await ogm.init();
-    const context = {
-      user,
-      token: createJWT(user),
-    };
 
     const handleRecords = async (activities) => {
       // sync activities to the graph db
@@ -44,46 +34,6 @@ export default async function handler(req, res) {
       await session.writeTransaction(async (tx) => {
         activities = await syncActivities({ tx, project, activities });
       });
-
-      // create embeddings if the project supports it
-      if (aiReady(project)) {
-        // map the incoming activities to unique conversationIds
-        var conversations = {};
-        var conversationIds = activities.map(
-          ({ conversation }) => conversation.id
-        );
-
-        // iterate over the conversationIds and load the activities for each one
-        for (let conversationId of conversationIds) {
-          const result = await Project.find({
-            where: { id: projectId },
-            selectionSet: `
-            {
-              conversations(where: { id: "${conversationId}" }) {
-                id
-                descendants(options: { sort: { timestamp: ASC } }) {
-                  id
-                  source
-                  sourceChannel
-                  globalActorName
-                  textHtml
-                  timestamp
-                }
-              }
-            }`,
-            context,
-          });
-          const [
-            {
-              conversations: [{ descendants: conversationActivities }],
-            },
-          ] = result;
-          conversations[conversationId] = conversationActivities;
-        }
-
-        // index conversations
-        await indexConversations({ project, conversations });
-      }
     };
 
     const { activities } = await fetchActivitiesPage({
